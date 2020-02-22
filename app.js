b647f3c177c914a3c663b3511491c2778fd63129
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-02-22 10:09:25 -0800

    GET request for all reviews

--- app.js
+++ app.js
@@ -6,8 +6,17 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 
+// OUR MOCK ARRAY OF PROJECTS
+let reviews = [
+  { title: "Great Review", movieTitle: "Batman II" },
+  { title: "Great Review", movieTitle: "Batman II" },
+  { title: "Great Review", movieTitle: "Batman II" },
+  { title: "Awesome Movie", movieTitle: "Titanic" }
+]
+
+// INDEX
 app.get('/', (req, res) => {
-    res.render("home", { msg: "Handlebars are Cool!" });
+  res.render('reviews-index', { reviews: reviews });
 })
 
 app.listen(3000, () => {
