7798dbaad691a7109ef54ba6db2689d1cdd1c65a
Makhmud Islamov <sunnatovichvv@gmail.com>
2020-02-22 10:00:53 -0800

    first GET request with basic handlebars

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render("home", { msg: "Handlebars are Cool!" });
 })
 
 app.listen(3000, () => {
